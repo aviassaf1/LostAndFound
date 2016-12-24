3b4c175e41552d450f6f3ae974ecdac9f261a941
aviassaf <guy30aviassaf@gmail.com>
2016-12-24 17:43:23 +0200

    start db

---
+++ LostAndFound/LostAndFound/Cache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound
+{
+    class Cache
+    {
+        //private Dictionary<string, User> _users;
+        private static Cache singleton;
+
+        private Cache()
+        {
+            //_users = new Dictionary<string, User>();
+        }
+
+        public static Cache getInstance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new Cache();
+                }
+                return singleton;
+            }
+        }
+
+        public void clear()
+        {
+//            if (this._forums != null)
+//                this._forums.Clear();            
+
+        }
+    }
+}
