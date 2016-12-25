0024817d3cd71892d5f13ad5efd446fd20dcbc46
rozentom <rozentom@post.bgu.ac.il>
2016-12-25 12:04:38 +0200

    creating connection

---
+++ LostAndFound/LostAndFound/SqlServerDBConection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LostAndFound
+{
+    class SqlServerDBConection
+    {
+        public static void Main()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "data source = LAPTOP-2CIHG2T7\\SQLEXPRESSWORK; database = LostAndFoundDataBase; integrated security = SSPI";
+            conn.Open();
+        }
+    }
+}
