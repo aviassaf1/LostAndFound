08cb15970bee3e29d7fb2e4a173274e9a5685478
rozentom <rozentom@post.bgu.ac.il>
2017-01-11 16:03:29 +0200

    plzplz work

--- LostAndFound/LostAndFound/SqlServerDBConection.cs
+++ LostAndFound/LostAndFound/SqlServerDBConection.cs
@@ -11,14 +11,29 @@ namespace LostAndFound
     {
         public static void Main()
         {
-            LostAndFoundDataBaseEntities db = new LostAndFoundDataBaseEntities();
-            db.Configuration.ProxyCreationEnabled = false;
-            db.Configuration.LazyLoadingEnabled = false;
+            /* LostAndFoundDataBaseEntities db = new LostAndFoundDataBaseEntities();
+             db.Configuration.ProxyCreationEnabled = false;
+             db.Configuration.LazyLoadingEnabled = false;
             foreach(var company in db.Companies)
             {
                 Console.WriteLine(company.userName);
             }
             while (true) ;
+    */
+            SqlConnection conn = new SqlConnection("user id=LAPTOP-2CIHG2T7\\Tomer;" + "password=password;" + "server=LAPTOP-2CIHG2T7\\SQLEXPRESSWORK;"
+                                                   + "Trusted_Connection=yes;" + "database=LostAndFoundDataBase;"
+                                                   + "connection timeout=30");
+            try
+            {
+                conn.Open();
+                Console.WriteLine("worked");
+                while (true) ;
+            }
+            catch
+            {
+                Console.WriteLine("didnt work");
+                while (true) ;
+            }
         }
     }
 }
