ae10392a6a46e9575417641c3354b305086b41c5
rozentom <rozentom@post.bgu.ac.il>
2017-01-13 14:56:56 +0200

    finally

---
+++ LostAndFound/DataLayer/Database.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Database
+    {
+        private static Database singleton;
+        LostAndFoundDBEntities db;
+
+        private Database()
+        {
+
+        }
+
+
+        public static Database getInstance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new Database();
+                    singleton.initializeDB();
+                }
+                return singleton;
+            }
+        }
+        private Boolean initializeDB()
+        {
+            try
+            {
+                this.db = new LostAndFoundDBEntities();
+                string Path = Environment.CurrentDirectory;
+                string[] appPath = Path.Split(new string[] { "bin" }, StringSplitOptions.None);
+                AppDomain.CurrentDomain.SetData("DataDirectory", appPath[0]);
+                //initialAddToCache();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public void testop()
+        {
+            foreach (var item in db.User)
+            {
+                Console.WriteLine(item.UserName);
+            }
+            while (true) ;
+        }
+        public void clear()
+        {
+            try
+            {
+                
+                //cache.clear();
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
---
+++ LostAndFound/DataLayer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db = Database.getInstance;
+            // ent.
+            //ent.Database.Connection.Open();
+            db.testop();
+        }
+    }
+}
