0bdb499103a65895dcd0082a1ec10c60d2d2f84c
נעם ברקאי < >
2017-01-16 22:38:25 +0200

    merge stuff

--- LostAndFound/Domain/Class1.cs
+++ LostAndFound/Domain/Class1.cs
@@ -14,7 +14,7 @@ namespace Domain
         {
             List<FBItem> list =  MatchManager.getInstance.getPostsFromGroup("EAACEdEose0cBAF4oCerYDZA1f7hPgkAkCZApnlwG6NGmUMSAt3WZCw3hkNUGxyp2PwKUDQO1K46GNUyP7REFx1sbJgPyw5DV9kyV2VOBZBIQ2c1EsRe4EZB1w5ZCqIMhkbPLqjVFkYlW4qiqcXvssZBR6FvDV9dhiso69CpGsfRAgZDZD",
                 "1538105046204967");
-            int i = 0;
+//            int i = 0;
         }
     }
 }
