bbd9690a6e233895a13968044801aaf0f383a6e6
rozentom <rozentom@post.bgu.ac.il>
2017-01-20 04:47:27 +0200

    k

--- LostAndFound/Test/UnitTests/DatabaseUnitTests.cs
+++ LostAndFound/Test/UnitTests/DatabaseUnitTests.cs
@@ -324,12 +324,44 @@ namespace Test.UnitTests
         }
 
         /******************************items tests***********************************/
+        //////////////////////////find item
         [TestMethod]
         public void findIdTests()
         {
             bool isWork = db.findIdTests();
             Assert.IsTrue(isWork);
         }
+        /// <summary>
+        /// //////////////////////remove item
+        /// </summary>
+        [TestMethod]
+        public void removeFoundItem()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsTrue(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNotNull(item);
+        }
+        [TestMethod]
+        public void removeLostItem()
+        {
+            int ret = db.addLostItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsTrue(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNotNull(item);
+        }
+        [TestMethod]
+        public void removeFBItem()
+        {
+            int ret = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            bool cond = ret > 0;
+            Assert.IsTrue(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNotNull(item);
+        }
+
         /******************************FB items tests***********************************/
 
         ///////////////add FB item
@@ -358,8 +390,312 @@ namespace Test.UnitTests
             Assert.AreEqual(item.publisherName, "1");
             Assert.AreEqual(item.type, "1");
         }
+        /******************************found items tests***********************************/
+        ///////////////add found item
+        [TestMethod]
+        public void addFoundItemValidExistCompany()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsTrue(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNotNull(item);
+        }
+        [TestMethod]
+        public void addFoundItemValidNotExistCompany()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validNotExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsFalse(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNull(item);
+        }
+        [TestMethod]
+        public void addFoundItemNotValidCompany()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, notValidCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsFalse(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNull(item);
+        }
+
+        ///////////////update found item
+        [TestMethod]
+        public void updateFoundItem()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            string update = db.updateFoundItem(ret, new System.Collections.Generic.List<string>(), "", DateTime.Now, "", "", "", false);
+            Assert.AreEqual("true", update);
+            FoundItems item = db.findItemByItemId(ret).CompanyItems.FoundItems;
+            Assert.IsNotNull(item.colors);
+            Assert.AreEqual(item.itemType, "");
+            Assert.AreEqual(item.location, "");
+            Assert.AreEqual(item.delivered, false);
+            Assert.AreEqual(item.photoLocation, "");
+            Assert.AreEqual(item.description, "");
+        }
+
+        /******************************lost items tests***********************************/
+        ///////////////add lost item
+        [TestMethod]
+        public void addLostItemValidExistCompany()
+        {
+            int ret = db.addLostItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsTrue(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNotNull(item);
+        }
+        [TestMethod]
+        public void addLostItemValidNotExistCompany()
+        {
+            int ret = db.addLostItem(null, null, DateTime.Now, null, null, 0, validNotExistCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsFalse(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNull(item);
+        }
+        [TestMethod]
+        public void addLostItemNotValidCompany()
+        {
+            int ret = db.addLostItem(null, null, DateTime.Now, null, null, 0, notValidCompanyName, null, null, null, true);
+            bool cond = ret > 0;
+            Assert.IsFalse(cond);
+            Items item = db.findItemByItemId(ret);
+            Assert.IsNull(item);
+        }
+
+        ///////////////update lost item
+        [TestMethod]
+        public void updateLostItem()
+        {
+            int ret = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            string update = db.updateFoundItem(ret, new System.Collections.Generic.List<string>(), "", DateTime.Now, "", "", "", false);
+            Assert.AreEqual("true", update);
+            FoundItems item = db.findItemByItemId(ret).CompanyItems.FoundItems;
+            Assert.IsNotNull(item.colors);
+            Assert.AreEqual(item.itemType, "");
+            Assert.AreEqual(item.location, "");
+            Assert.AreEqual(item.delivered, false);
+            Assert.AreEqual(item.photoLocation, "");
+            Assert.AreEqual(item.description, "");
+        }
+
+        /******************************matches tests***********************************/
+
+        ///////////////add match
+        [TestMethod]
+        public void add_match_existingCompanyItem_ExistingItem()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsTrue(cond);
+            Matches match = db.findMathByMatchId(matchId);
+            Assert.IsNotNull(match);
+            Assert.AreEqual(cItemId, match.CompanyItems.itemId);
+            Assert.AreEqual(itemId, match.Items.itemID);
+        }
+        [TestMethod]
+        public void add_match_notValidCompanyItem_ExistingItem()
+        {
+            int cItemId = -1;
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_notExistingCompanyItem_ExistingItem()
+        {
+            int cItemId = 934892;
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_existingCompanyItem_notExistingItem()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = 57483;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_notValidCompanyItem_notExistingItem()
+        {
+            int cItemId = -1;
+            int itemId = 57483;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_notExistingCompanyItem_notExistingItem()
+        {
+            int cItemId = 934892;
+            int itemId = 57483;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_existingCompanyItem_notValidItem()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = -1;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_notValidCompanyItem_notValidItem()
+        {
+            int cItemId = -1;
+            int itemId = -1;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
+        [TestMethod]
+        public void add_match_notExistingCompanyItem_notvalidItem()
+        {
+            int cItemId = 934892;
+            int itemId = -1;
+            int matchId = db.addMatch(cItemId, itemId, "");
+            bool cond = matchId > 0;
+            Assert.IsFalse(cond);
+        }
 
 
+        ////////////////////////remove match
+        [TestMethod]
+        public void removeMatch()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            int matchId = db.addMatch(cItemId, itemId, "");
+            db.removeMatch(matchId);
+            Matches match = db.findMathByMatchId(matchId);
+            Assert.IsNull(match);
+        }
+
+        ////////////////////////update match
+        [TestMethod]
+        public void updateMatch()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            int matchId = db.addMatch(cItemId, itemId, "");
+            db.updateMatch(matchId, "hello");
+            Matches match = db.findMathByMatchId(matchId);
+            Assert.IsNotNull(match);
+            Assert.AreEqual("hello", match.matchStatus);
+        }
+
+        ////////////////////////update match
+        [TestMethod]
+        public void findMatchValidExistId()
+        {
+            int cItemId = db.addFoundItem(null, null, DateTime.Now, null, null, 0, validExistCompanyName, null, null, null, true);
+            int itemId = db.addFBItem(null, null, DateTime.Now, null, null, null, null, null);
+            int matchId = db.addMatch(cItemId, itemId, "");
+            Matches match = db.findMathByMatchId(matchId);
+            Assert.IsNotNull(match);
+        }
+        [TestMethod]
+        public void findMatchValidNotExistId()
+        {
+            Matches match = db.findMathByMatchId(337484);
+            Assert.IsNull(match);
+        }
+        [TestMethod]
+        public void findMatchNotValidId()
+        {
+            Matches match = db.findMathByMatchId(-1);
+            Assert.IsNull(match);
+        }
+
+        /******************************user tests***********************************/
+        //////////////////////find user
+        [TestMethod]
+        public void find_user_valid_andExist()
+        {
+            User user = db.findUserByUserName(validExistUserName);
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user.UserName, validExistUserName);
+        }
+        [TestMethod]
+        public void find_user_valid_andNotExist()
+        {
+            User user = db.findUserByUserName(validNotExistUserName);
+            Assert.IsNull(user);
+        }
+        [TestMethod]
+        public void find_user_not_valid()
+        {
+            User user = db.findUserByUserName(notValidUserName);
+            Assert.IsNull(user);
+        }
+
+
+        //////////////////////add user
+        [TestMethod]
+        public void add_user_notExist_valid()
+        {
+            string str = db.addUser(validNotExistUserName, "", true);
+            User user = db.findUserByUserName(validNotExistUserName);
+            Assert.AreEqual(str, "true");
+            Assert.IsNotNull(user);
+        }
+        [TestMethod]
+        public void add_user_Exist_valid()
+        {
+            string str = db.addUser(validExistUserName, "", true);
+            User user = db.findUserByUserName(validExistUserName);
+            Assert.AreNotEqual(str, "true");
+        }
+        [TestMethod]
+        public void add_user_userName_notValid()
+        {
+            string str = db.addUser(notValidUserName, "", true);
+            Assert.AreNotEqual(str, "true");
+        }
+        [TestMethod]
+        public void remove_user_valid_exist()
+        {
+            string str = db.removeUser(validExistUserName);
+            Assert.AreEqual("true", str);
+            User user = db.findUserByUserName(validExistUserName);
+            Assert.IsNull(user);
+        }
+        [TestMethod]
+        public void remove_user_valid_notExist()
+        {
+            string str = db.removeUser(validNotExistUserName);
+            Assert.AreNotEqual("true", str);
+        }
+        [TestMethod]
+        public void remove_user_NotValid()
+        {
+            string str = db.removeUser(notValidUserName);
+            Assert.AreNotEqual("true", str);
+        }
+
+        /////////////////////update user
+        [TestMethod]
+        public void update_user()
+        {
+            string str = db.updateUser(validExistUserName, "new");
+            Assert.AreEqual(str, "true");
+            User user = db.findUserByUserName(validExistUserName);
+            Assert.AreEqual(user.password, "new");
+        }
 
     }
 }
