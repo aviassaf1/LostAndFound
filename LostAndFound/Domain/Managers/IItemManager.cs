09e6f838a789cd55d8b75fd85882bb432f7536b3
נעם ברקאי < >
2017-03-06 16:34:03 +0200

    company manager tests

--- LostAndFound/Domain/Managers/IItemManager.cs
+++ LostAndFound/Domain/Managers/IItemManager.cs
@@ -9,7 +9,7 @@ namespace Domain.Managers
 {
     public interface IItemManager
     {
-        List<CompanyItem> getAllCompanyItems(String companyName);
+        List<CompanyItem> getAllCompanyItems(String companyName); //returns null if name is not valid
         String addLostItem(List<string> sColors, string sType, DateTime date, string location, string description,
             int serialNumber, string companyName, string contactName, string contactPhone, string photoLocation, string token);
         String addFoundItem(List<string> sColors, string sType, DateTime date, string location, string description,
