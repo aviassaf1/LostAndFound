dc5dd77277e4f8543ea52b2980b0f799597457fa
נעם ברקאי < >
2017-03-31 14:27:20 +0300

    client data objects

---
+++ LostAndFound/WebHost/DataObjects/FoundItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHost.DataObjects
+{
+    public class FoundItem : CompanyItem
+    {
+    }
+}
